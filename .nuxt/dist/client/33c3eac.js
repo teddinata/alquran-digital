8eddd69ce9981a77d89bf57d9c2338c99559d05c
teddinata <teddinata@users.noreply.github.com>
2024-01-12 14:10:40 +0700

    fix: pwa

---
+++ .nuxt/dist/client/33c3eac.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{307:function(n,t,e){"use strict";e.r(t);var l={name:"NuxtTutorial"},o=e(51),component=Object(o.a)(l,(function(){return(0,this._self._c)("div")}),[],!1,null,null,null);t.default=component.exports}}]);
